1f7f0f13ea3a0ea90bd314f31700816994c8fb44
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-08-09 14:34:46 -0300

    Creo un contador basico, usando un StatefullWidget

---
+++ StatefullWidget/Contador/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+//Creo una clase app que retorne un MarialApp Widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StatefulWidget Example',
+      home: CounterApp(),
+    );
+  }
+}
+//Creo una clase Contador que herede StatefullWidget
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() {
+    return _CounterAppState();
+  }
+}
+//Creo una clase privada con _ que herede de State<CounterApp>, esta clase contiene el estado mutable (valor del contador) y un metodo para modificarlo
+class _CounterAppState extends State<CounterApp> {
+  int _counter = 0;
+//Definimos un metodo para que aumenta el contador en uno
+  void _incrementCounter() {
+    //utilizamos el metodo setState para decirle a Flutter que el estado ha cambiado y la interfaz de usuario necesita actualizarse
+    setState(() {
+      _counter++;
+    });
+  }
+//En el metodo build() construimos la interfaz de usuario utilizando el valor actual de contador y un boton flotante que llama a _incrementCounter() cuando se presiona
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Hola, el contador es $_counter")),
+      floatingActionButton: FloatingActionButton(
+        //Aca decimos que al presionar el boton se llame al metodo de incrementar contador
+        onPressed: _incrementCounter,
+        tooltip: 'Incrementar',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
