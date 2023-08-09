fcb4f777c3a4fd57a162b6593b1f3da30ddff7f7
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-08-09 13:54:50 -0300

    Se crea una clase de autentificar usando InheritedWidgets

--- InheritedWidgets/AutentificadorBasico/main.dart
+++ InheritedWidgets/AutentificadorBasico/main.dart
@@ -3,26 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+//Clase para representar la informacion de autentificacion
 class AuthData {
   final bool isAuthenticated;
   final String username;
 
   AuthData({required this.isAuthenticated, required this.username});
 }
-
+//Este widget almacena la informacion de autentificacion que queremos compartir en toda la aplicacion
 class AuthDataProvider extends InheritedWidget {
   final AuthData authData;
 
   AuthDataProvider({required this.authData, required Widget child})
       : super(child: child);
 
+  //Metodo para verificar si un Widget dependiente necesita reconstruirse
   @override
   bool updateShouldNotify(covariant AuthDataProvider oldWidget) {
     throw authData.isAuthenticated != oldWidget.authData.isAuthenticated ||
         authData.username != oldWidget.authData.username;
   }
 
+  //Metodo estatico para acceder al InheritedWidget desde el contexto
   static AuthDataProvider of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType()!;
   }
@@ -31,6 +33,7 @@ class AuthDataProvider extends InheritedWidget {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Creamos una instancia de AuthData y la pasamos a AuthDataProvider
     final authData = AuthData(isAuthenticated: true, username: 'exampleuser');
 
     return AuthDataProvider(
@@ -45,6 +48,7 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Usamos AuthDataProvider.of(context) para acceder a la informacion de autentificacion y mostrar mensajes personalizados segun el estado de autentificacion.
     final authData = AuthDataProvider.of(context).authData;
 
     return Scaffold(
