84a081576040a395e72660056070b90aef72dcd0
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-08-28 11:29:51 -0300

    Añado un ejemplo basico de StatefullWidget con un contador

--- StatefullWidget/Contador/main.dart
+++ StatefullWidget/Contador/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 //Creo una clase app que retorne un MarialApp Widget
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'StatefulWidget Example',
       home: CounterApp(),
     );
@@ -15,6 +17,8 @@ class MyApp extends StatelessWidget {
 }
 //Creo una clase Contador que herede StatefullWidget
 class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
   @override
   _CounterAppState createState() {
     return _CounterAppState();
@@ -39,7 +43,7 @@ class _CounterAppState extends State<CounterApp> {
         //Aca decimos que al presionar el boton se llame al metodo de incrementar contador
         onPressed: _incrementCounter,
         tooltip: 'Incrementar',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
