12b6507be172a08a126194490bc0ce375b7c8c3d
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-09-02 01:07:42 -0300

    Creo app de navegacion basica entre pantallas

--- NavigationAPI/NavegacionBasica/main.dart
+++ NavigationAPI/NavegacionBasica/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(const MaterialApp(
+runApp( MaterialApp(
   home: MenuScreen(),
   title: "Navigation Example",
+  //Declaro las rutas de la aplicacion
+  routes: <String, WidgetBuilder> {
+    '/perfil_usuario': (context) => PerfilUsuario()
+  },
   )
 );
 }
@@ -18,14 +22,25 @@ class MenuScreen extends StatelessWidget {
         title: const Text("Menu Principal"),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text("Abir pantalla Siguiente"),
-          onPressed: () {
-            //Navegar entre pantallas
-            Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
-          },
-        ),
-      ),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text("Abir Segunda Pantalla"),
+              onPressed: () {
+                //Navegar entre pantallas
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+              },
+            ),
+            ElevatedButton(
+              child: const Text("Abir Pantalla Perfil Usuario"),
+              onPressed: () {
+                //Navego en una pantalla llamada perfil_usuario
+                Navigator.pushNamed(context, "/perfil_usuario");
+              }
+            ),
+          ],
+        )
+     )
     );
   }
 }
@@ -50,3 +65,28 @@ class SecondScreen extends StatelessWidget {
     );
   }
 }
+
+class PerfilUsuario extends StatelessWidget {
+  const PerfilUsuario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Perfil Usuario"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+            const Text("Maximiliano"),
+            ElevatedButton(onPressed: () {
+              Navigator.pop(context);
+            }, child: const Text("Volver"))
+          ],
+        ),
+      )
+    );
+  }
+}
+
